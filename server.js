482263f478143d6ab629d155088e72eec74d2f64
kaysongelsleichter-tech <kayson_gelsleichter@estudante.sesisenai.org.br>
2026-02-13 09:29:16 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+import fastify from 'fastify';
 
+const server = fastify();
+
+server.get('/usuarios' , () => {
+        return 'Ola Mundo!'
+}) 
+
+server.post('/usuarios' , (request, reply) => {
+        return 'Ola Mundo!'
+})
+
+server.listen({
+    port:3000
+})
